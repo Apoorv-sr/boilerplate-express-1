5d3de375ef47cde76fcc0a8c33ce0ab333e1b3a9
Apoorava singh <109285079+Apoorv-sr@users.noreply.github.com>
2022-10-06 12:30:04 +0000

    completed Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
 let express = require('express');
 let app = express();
 
-
+// const mySecret = process.env['MESSAGE_STYLE']
 
 //see we can also use the route handlers i.e app.get as APIs,apis are nothing but the get,post,put,delete.. requests to the backend and we receive the request and do something either we communicate to the database or we do some calculation on the backend i.e. backend machine so this is an api,now also note that the ip address is equivalent to "/" i.e. in our local computer/server/host machine its ip we know is localhost or 127.0.0.1 and it is equivalent to "/",similarly the remote computers/servers like google.com also means "/" path for the google backend server and it responds with an html file the famous google searchbar.
 app.get("/json",function(req,res)
         {
-          res.json({"message":"Hello json"})
+          res.json((process.env.MESSAGE_STYLE==="uppercase")?{"message":"HELLO JSON"}:{"message":"hello json"})
         })
 
 //see the below app.use method is used to mount express.static middleware so that we can use the static files like images and styles.css files used in our html files,so thae first parameter is the path for which express.static will be executed,so whenever in the html file we will be trying to access the static files like images and css we will give some link,so see in the html file we have a static css link i.e. <link rel="stylesheet" hreff="/public/style.css">,now see here href wants to acess /public part so this matches the starting substring "/public",so express.static will be executed and in the static method we have to pass the absolute path of the folder where the static files are kept,so we have given the absolute path of the public folder
